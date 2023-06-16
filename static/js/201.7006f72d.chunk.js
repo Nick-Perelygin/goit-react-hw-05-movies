6083f2a69a076b34f4595951216e28a6fc4efb24
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-06-16 20:12:07 +0000

    Deploying to gh-pages from @ Nick-Perelygin/goit-react-hw-05-movies@0f69b2e54d003fc0d444999a272425ad94f6f410 🚀

---
+++ static/js/201.7006f72d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[201],{201:function(e,n,t){t.r(n),t.d(n,{default:function(){return r}});var u=t(184);function r(){return(0,u.jsx)("p",{children:"Page no found"})}}}]);
+//# sourceMappingURL=201.7006f72d.chunk.js.map
